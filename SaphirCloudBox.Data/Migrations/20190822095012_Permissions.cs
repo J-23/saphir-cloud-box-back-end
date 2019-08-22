0a995243a9299f3966a41ce9910ac2d808558141
SonyaKovtunova <sofi.kovtunova@gmail.com>
2019-08-22 12:51:35 +0300

    permission migration

---
+++ SaphirCloudBox.Data/Migrations/20190822095012_Permissions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SaphirCloudBox.Data.Migrations
+{
+    public partial class Permissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileStorageAccessUser");
+
+            migrationBuilder.CreateTable(
+                name: "FileStoragePermission",
+                columns: table => new
+                {
+                    FileStorageId = table.Column<int>(nullable: false),
+                    RecipientId = table.Column<int>(nullable: false),
+                    SenderId = table.Column<int>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileStoragePermission", x => new { x.FileStorageId, x.RecipientId });
+                    table.ForeignKey(
+                        name: "FK_FileStoragePermission_FileStorage_FileStorageId",
+                        column: x => x.FileStorageId,
+                        principalTable: "FileStorage",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_FileStoragePermission_AspNetUsers_RecipientId",
+                        column: x => x.RecipientId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_FileStoragePermission_AspNetUsers_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileStoragePermission_RecipientId",
+                table: "FileStoragePermission",
+                column: "RecipientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileStoragePermission_SenderId",
+                table: "FileStoragePermission",
+                column: "SenderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileStoragePermission");
+
+            migrationBuilder.CreateTable(
+                name: "FileStorageAccessUser",
+                columns: table => new
+                {
+                    FileStorageId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileStorageAccessUser", x => new { x.FileStorageId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_FileStorageAccessUser_FileStorage_FileStorageId",
+                        column: x => x.FileStorageId,
+                        principalTable: "FileStorage",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_FileStorageAccessUser_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileStorageAccessUser_UserId",
+                table: "FileStorageAccessUser",
+                column: "UserId");
+        }
+    }
+}
