1c284d23153e9dcfb595a41db40524534df013cd
SonyaKovtunova <sofi.kovtunova@gmail.com>
2019-08-26 15:12:21 +0300

    global exception

---
+++ SaphirCloudBox.Host/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using SaphirCloudBox.Host.Infractructure;
+using SaphirCloudBox.Services.Contracts.Exceptions;
+
+namespace SaphirCloudBox.Host.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+
+            var message = string.Empty;
+
+            if (exception is NotFoundException)
+            {
+                message = ResponseMessage.NOT_FOUND.ToString();
+            }
+
+            return context.Response.WriteAsync(message);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
